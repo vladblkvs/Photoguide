baedfa11525a86a0592568a074acfed5d8f4d3cc
vladblkvs <gremlinux@gmail.com>
2020-03-13 01:45:06 +0300

    Add cover styles for desktop

--- gulpfile.js
+++ gulpfile.js
@@ -48,7 +48,7 @@ gulp.task("css", function() {
 // });
 
 gulp.task("sprite", function() {
-  return gulp.src("source/img/**/*.svg")
+  return gulp.src("source/img/sprite/*.svg")
     .pipe(svgstore({
       inlineSvg: true
     }))

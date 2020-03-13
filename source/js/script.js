1c7fb467a443a05f4ecc0a736d0c2c88cfc31981
vladblkvs <gremlinux@gmail.com>
2020-03-13 19:21:01 +0300

    Add gallery

--- source/js/script.js
+++ source/js/script.js
@@ -20,3 +20,38 @@ var hideMenu = function () {
   header.classList.remove('page-header--expanded');
   header.classList.remove('page-header--closed');
 };
+
+var photoBlock = document.querySelector('.gallery__photos');
+var galleryLeftPart = photoBlock.querySelector('.gallery__inner--left');
+var galleryRightPart = photoBlock.querySelector('.gallery__inner--right');
+var GAP = 20;
+var PADDING = 80 * 2;
+
+var movePhotosToLeft = function () {
+  if (photoBlock.offsetWidth >= 1200) {
+    galleryLeftPart.classList.remove('gallery__inner--slide-right');
+    galleryRightPart.classList.remove('gallery__inner--slide-right');
+    galleryLeftPart.classList.add('gallery__inner--slide-left');
+    galleryRightPart.classList.add('gallery__inner--slide-left');
+    var shift = (photoBlock.offsetWidth - PADDING) - (galleryLeftPart.offsetWidth + GAP + galleryRightPart.offsetWidth);
+    var transform = 'translateX('+ shift + 'px)';
+    galleryLeftPart.style.transform = transform;
+    galleryRightPart.style.transform = transform;
+    galleryRightPart.removeEventListener('mouseover', movePhotosToLeft);
+    galleryLeftPart.addEventListener('mouseover', movePhotosToRight);
+  }
+};
+
+var movePhotosToRight = function () {
+  galleryLeftPart.classList.remove('gallery__inner--slide-left');
+  galleryRightPart.classList.remove('gallery__inner--slide-left');
+  galleryLeftPart.classList.add('gallery__inner--slide-right');
+  galleryRightPart.classList.add('gallery__inner--slide-right');
+  galleryLeftPart.style.transform = '';
+  galleryRightPart.style.transform= '';
+  galleryRightPart.removeEventListener('mouseover', movePhotosToRight);
+  galleryRightPart.addEventListener('mouseover', movePhotosToLeft);
+}
+
+galleryRightPart.addEventListener('mouseover', movePhotosToLeft);
+
